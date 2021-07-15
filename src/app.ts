1932289d835c102885cdbd72015b6b2a651634e0
Oxana Danilova <oxanadanilova11@gmail.com>
2021-07-15 19:13:30 +0200

    refactor:refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import categories from './categories/router';
 
-const port = process.env.PORT || 3000
-
 
 const app = express();
 app.use(cors());
 app.use('/api/categories', categories);
 
-app.listen(port, ()=>console.log('Server started on '+port));
+app.listen(3000, ()=>console.log('Server started on 3000'));
