5feea892db94600b2590c939dea89de48a53949b
Oxana Danilova <oxanadanilova11@gmail.com>
2021-07-15 17:44:32 +0200

    fix:fix port

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import categories from './categories/router';
 
+const port = process.env.PORT || 3000
+
 
 const app = express();
 app.use(cors());
 app.use('/api/categories', categories);
 
-app.listen(3000, ()=>console.log('Server started on 3000'));
+app.listen(port, ()=>console.log('Server started on '+port));
