e436ccbbd044d6bdfbd71f3842a8f2f5d3574f03
Oxana Danilova <oxanadanilova11@gmail.com>
2021-07-15 22:55:22 +0200

    fix: fix settings in package.json

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import categories from './categories/router';
 
+const PORT : string|number = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use('/api/categories', categories);
 
-app.listen(3000, ()=>console.log('Server started on 3000'));
+app.listen(PORT, ()=>console.log('Server started on'+PORT));
